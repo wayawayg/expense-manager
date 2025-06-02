008c7b404d55fbb623ecf196c1eba8f1cbf4c633
Ivan <idev.wayawayg@gmail.com>
2025-06-02 21:41:25 +0300

    feat(infra): add mongodb repositories

--- src/common/config.py
+++ src/common/config.py
@@ -7,12 +7,20 @@ load_dotenv()
 
 
 @dataclass(eq=False, frozen=True)
+class Mongo:
+    url: str = os.environ["MONGO_URL"]
+    database: str = os.environ["MONGO_DATABASE_NAME"]
+    expense_collection: str = os.environ["MONGO_EXPENSE_COLLECTION_NAME"]
+
+
+@dataclass(eq=False, frozen=True)
 class Telegram:
     token: str = os.environ["TELEGRAM_BOT_TOKEN"]
 
 
 @dataclass(eq=False, frozen=True)
 class Config:
+    mongo: Mongo = Mongo()
     telegram: Telegram = Telegram()
 
 
