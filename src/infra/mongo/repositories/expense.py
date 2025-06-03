09c6b60809f4d3be20515af89f3e8322d14b20be
Ivan <idev.wayawayg@gmail.com>
2025-06-03 16:54:36 +0300

    feat(infra): add get methods to mongo repository

--- src/infra/mongo/repositories/expense.py
+++ src/infra/mongo/repositories/expense.py
@@ -12,6 +12,17 @@ from src.infra.mongo.exceptions.expense import DatabaseWriteException
 class MongoExpenseRepository:
     _collection: AsyncIOMotorCollection
 
+    @staticmethod
+    def _conver_mongo_response_to_entity(data: dict) -> Expense:
+        to_convert = data.copy()
+        to_convert["amount"] = data["amount"].to_decimal()
+        to_convert["date"] = datetime.date(
+            year=data["date"].year,
+            month=data["date"].month,
+            day=data["date"].day,
+        )
+        return Expense.model_validate(to_convert, from_attributes=True)
+
     async def add(
         self,
         expense: Expense,
@@ -49,4 +60,52 @@ class MongoExpenseRepository:
                 day=expense["date"].day,
             )
             result.append(Expense.model_validate(expense, from_attributes=True))
+        return result
+
+    async def get_by_category(
+        self,
+        user_id: int,
+        category: str,
+    ) -> list[Expense]:
+        cursor = self._collection.find(
+            {
+                "user_id": user_id,
+                "category": category,
+            }
+        )
+        result = []
+        async for document in cursor:
+            expense = self._conver_mongo_response_to_entity(document)
+            result.append(expense)
+        return result
+
+    async def get_by_date_period(
+        self,
+        user_id: int,
+        start_date: datetime.date,
+        end_date: datetime.date,
+    ) -> list[Expense]:
+        cursor = self._collection.find(
+            {
+                "user_id": user_id,
+                "date": {
+                    "$gte": datetime.datetime(
+                        year=start_date.year,
+                        month=start_date.month,
+                        day=start_date.day,
+                        tzinfo=datetime.UTC,
+                    ),
+                    "$lte": datetime.datetime(
+                        year=end_date.year,
+                        month=end_date.month,
+                        day=end_date.day,
+                        tzinfo=datetime.UTC,
+                    ),
+                },
+            }
+        )
+        result = []
+        async for document in cursor:
+            expense = self._conver_mongo_response_to_entity(document)
+            result.append(expense)
         return result
