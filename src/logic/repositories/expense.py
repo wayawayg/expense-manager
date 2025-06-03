737505d0207761f279e2b53d155bd19a1ec87ccf
Ivan <idev.wayawayg@gmail.com>
2025-06-03 14:18:18 +0300

    feat(logic): add methods of getting expenses by categories and periods

--- src/logic/repositories/expense.py
+++ src/logic/repositories/expense.py
@@ -1,21 +1,37 @@
 from dataclasses import dataclass
+from datetime import date
 from typing import Any, Protocol
 
 from src.domain.entities.expense import Expense
 
 
 class ExpenseRepository(Protocol):
-    def add(
+    async def add(
         self,
         expense: Expense,
     ) -> Expense:
         raise NotImplementedError
 
-    def get_all(
+    async def get_all(
         self,
         user_id: int,
         limit: int,
         offset: int,
+    ) -> list[Expense]:
+        raise NotImplementedError
+
+    async def get_by_category(
+        self,
+        user_id: int,
+        category: str,
+    ) -> list[Expense]:
+        raise NotImplementedError
+
+    async def get_by_date_period(
+        self,
+        user_id: int,
+        start_date: date,
+        end_date: date,
     ) -> list[Expense]:
         raise NotImplementedError
 
@@ -24,14 +40,14 @@ class ExpenseRepository(Protocol):
 class InMemoryExpenseRepository:
     _storage: list[Expense]
 
-    def add(
+    async def add(
         self,
         expense: Expense,
     ) -> Expense:
         self._storage.append(expense)
         return expense
 
-    def get_all(
+    async def get_all(
         self,
         user_id: int,
         limit: int,
@@ -42,3 +58,31 @@ class InMemoryExpenseRepository:
             if expense.user_id == user_id:
                 result.append(expense)
         return result[offset : offset + limit]
+
+    async def get_by_category(
+        self,
+        user_id: int,
+        category: str,
+    ) -> list[Expense]:
+        result = []
+        for expense in self._storage:
+            if expense.user_id == user_id and expense.category == category:
+                result.append(expense)
+        return result
+
+    async def get_by_date_period(
+        self,
+        user_id: int,
+        start_date: date,
+        end_date: date,
+    ) -> list[Expense]:
+        result = []
+        for expense in self._storage:
+            if (
+                expense.user_id == user_id
+                and expense.date
+                and expense.date >= start_date
+                and expense.date <= end_date
+            ):
+                result.append(expense)
+        return result
